9c522f442034497c581f6d54db75b73a6d27d3cf
Warren Loy Harper II <warrenharper2@att.net>
2019-07-19 20:30:51 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,4 @@
-def mario
-  phrase = 'it's a me, Mario!'
-  puts phrase
-end
-def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
+def any_phrase
+phrase = "Do A Barrel Roll!"
+puts phrase
 end
